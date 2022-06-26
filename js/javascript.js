f6ea6df06b9290d31350b9a414626ec4113a7524
davidriver <dav-rc@hotmail.com>
2022-06-26 14:23:08 -0500

    Commit before testing checkWin functions

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,16 @@
+//Factory for player objects
+const playerFactory = (char) => {
+    rowsContainer = [];
+    columnsContainer = [];
+    diagonalContainer = [];
+    oppositeDiagonalContainer = [];
+    return {char, rowsContainer, columnsContainer, diagonalContainer, oppositeDiagonalContainer};
+}
+
+//Initiate player objects
+const player1 = playerFactory('o');
+const player2 = playerFactory('x');
+
 //Module for gameboard object
 const gameBoard = (() => {
 
@@ -13,37 +26,44 @@ const gameBoard = (() => {
 //Module for displayController object
 const displayController = (() => {
     
-    let currentChar = 'x';
+    let currentPlayer = player1;
 
     const printBoard = (arr) => {
         let indexCounter = 0;
+        let rowCounter = 1;
+        let columnCounter = 1;
         for (let x=0; x<3; x++){
             let rowDiv = document.createElement('div');
             rowDiv.classList.add('row');
             for (let y=0; y<3; y++){
                 let div = document.createElement('div');
-                div.setAttribute('index', indexCounter)
+                div.setAttribute('index', indexCounter);
+                div.setAttribute('row', rowCounter);
+                div.setAttribute('column', columnCounter);
+                columnCounter++;
                 div.textContent = gameBoard.gameboard[indexCounter];
                 div.classList.add('grid-item');
                 div.addEventListener('click', function(){
                     if (!div.textContent){
-                        gameBoard.addToArray(div.getAttribute('index'), currentChar)
-                        addContent(this, currentChar);
-                        toggleChar();
+                        gameBoard.addToArray(div.getAttribute('index'), currentPlayer.char)
+                        addContent(this, currentPlayer.char);
+                        togglePlayer();
                     }
                 });
                 rowDiv.appendChild(div);
                 indexCounter++;
             }
+            columnCounter = 1;
             boardContainer.appendChild(rowDiv);
+            rowCounter++;
         }
     };
 
-    const toggleChar  = () => {
-        if (currentChar == 'x'){
-            currentChar = 'o';
+    const togglePlayer  = () => {
+        if (currentPlayer == player1){
+            currentPlayer = player2;
         } else {
-            currentChar = 'x';
+            currentPlayer = player1;
         }
     }
 
@@ -51,13 +71,8 @@ const displayController = (() => {
         div.textContent = content;
     }
 
-    return {printBoard, addContent, toggleChar};
+    return {printBoard, addContent, togglePlayer};
 })();
-
-//Factory for player objects
-const playerFactory = (name) => {
-    return {name};
-}
 
 boardContainer = document.querySelector('#board-container');
 
